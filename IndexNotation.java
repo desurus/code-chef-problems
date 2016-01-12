8a23e4b5db7a5a326f9a76335cc3d5773531b19e
Oleksandr Kryshchenko <Oleksandr.Kryshchenko@aexp.com>
2016-01-12 12:18:40 -0800

    Finished working solution for the ONP problem.

--- IndexNotation.java
+++ IndexNotation.java
@@ -1,52 +1,57 @@
+import java.lang.Character;
+import java.lang.StringBuilder;
 import java.util.*;
 
 /**
  * Problem https://www.codechef.com/problems/ONP
  */
 class IndexNotation {
-	/*
-	Gets equation from the input in the Index Notation (Polish Notation)
-	format and prints out the result.
-	*/
+	/**
+     * Gets equation from the input in the RPN (Reverse Polish Notation) format and prints out the result.
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Stack stack = new Stack();
-		List<String> operators = new ArrayList<String>();
-		operators.add("+");
-		operators.add("-");
-		operators.add("*");
-		operators.add("/");
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            List<String> result = new ArrayList<String>();
+            // looping over given number of tests
+            while (n >= 0) {
+                Stack<String> stack = new Stack<String>();
+                List<String> operators = new ArrayList<String>();
+                operators.add("+");
+                operators.add("-");
+                operators.add("*");
+                operators.add("/");
+                operators.add("^");
+                StringBuilder resultString = new StringBuilder();
 
-		if (sc.hasNextLine()) {
-			String line = sc.nextLine();
-			for (int i = (line.length() - 1); i >= 0; i--) {
-				String element = String.valueOf(line.charAt(i));
-				// his is operator and we need to pop two elements from stack
-				// use operator on them and put the result back in stack
-				if (operators.contains(element)) {
-					int operand1 = (int)stack.pop();
-					int operand2 = (int)stack.pop();
-					int result = 0;
-					if (element.equals("+")) {
-						result = operand1 + operand2;
-					}
-					if (element.equals("-")) {
-						result = operand1 - operand2;
-					}
-					if (element.equals("*")) {
-						result = operand1 * operand2;
-					}
-					if (element.equals("/")) {
-						result = operand1 / operand2;
-					}
-					// put the result back to stack
-					stack.push(result);
-				}
-				if (element.matches(".*\\d.*")) {
-					stack.push(Integer.parseInt(element));
-				}
-			}
-			System.out.println(stack.pop());
-		}
+                if (sc.hasNextLine()) {
+                    String line = sc.nextLine();
+                    for (int i = 0; i < line.length(); i++) {
+                        String element = String.valueOf(line.charAt(i));
+
+                        // if current element is letter add it to resulting string
+                        if (Character.isLetter(line.charAt(i))) {
+                            resultString.append(element);
+
+                            // if current element is operator we add it to the stack
+                        } else if (operators.contains(element)) {
+                            stack.push(element);
+
+                            // if current element is a closing bracket we need to move elements from stack to the result string
+                        } else if (element.equals(")")) {
+                            if (!stack.isEmpty()) {
+                                resultString.append(stack.pop());
+                            }
+                        }
+                    }
+                    result.add(resultString.toString());
+                    n--;
+                }
+            }
+            for (String line : result) {
+                System.out.println(line);
+            }
+        }
 	}
 }
