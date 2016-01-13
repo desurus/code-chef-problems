06a85f091326cd16d4375039cac250943da2f47b
Oleksandr Kryshchenko <Oleksandr.Kryshchenko@aexp.com>
2016-01-13 14:12:28 -0800

    Added blank class for the CielRcpt problem.

---
+++ CielRcpt.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/**
+ * Problem https://www.codechef.com/problems/CIELRCPT
+ */
+class CielRcpt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            List<String> result = new ArrayList<String>();
+            // looping over given number of tests
+            while (n >= 0) {
+
+            }
+        }
+    }
+}
