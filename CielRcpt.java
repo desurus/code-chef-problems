8b9594def848acaf732d84281046664f7b24a8f9
Oleksandr Kryshchenko <Oleksandr.Kryshchenko@aexp.com>
2016-01-13 15:09:05 -0800

    Added solution for the CielRcpt problem.

--- CielRcpt.java
+++ CielRcpt.java
@@ -1,3 +1,6 @@
+import java.lang.Character;
+import java.lang.Integer;
+import java.lang.String;
 import java.util.*;
 
 /**
@@ -6,12 +9,37 @@ import java.util.*;
 class CielRcpt {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int priceList[] = {1,2,4,8,16,32,64,128,256,512,1024,2048};
         if (sc.hasNextInt()) {
             int n = sc.nextInt();
-            List<String> result = new ArrayList<String>();
+            List<Integer> result = new ArrayList<Integer>();
             // looping over given number of tests
-            while (n >= 0) {
-
+            while (n > 0) {
+                int i;
+                if (sc.hasNextInt()) {
+                    int inputNumber = sc.nextInt();
+                    int originalInput = inputNumber;
+                    int count = 0;
+                    while (inputNumber > 0) {
+                        if (inputNumber > priceList[11]) {
+                            inputNumber = inputNumber - priceList[11];
+                            count++;
+                        } else {
+                            for (i = 0; i < 12; i++ ) {
+                                if (priceList[i] > originalInput) {
+                                    break;
+                                }
+                            }
+                            inputNumber = inputNumber - priceList[i - 1];
+                            count++;
+                        }
+                    }
+                    result.add(count);
+                    n--;
+                }
+            }
+            for (Integer line : result) {
+                System.out.println(line);
             }
         }
     }
